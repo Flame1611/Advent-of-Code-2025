ac50ae0065566a2b54937480bc8726ac05ad3745
Flame1611 <113800369+Flame1611@users.noreply.github.com>
2025-12-03 01:40:39 +0000

    Stuck on Part 2, Moving onto Day 2 to clear head

--- Day1/code.java
+++ Day1/code.java
@@ -2,11 +2,19 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-public class code{
+public class code {
     public static void main(String[] args) throws IOException {
+        Scanner file = new Scanner(Path.of("input.txt"));
+        System.out.printf("The password is:\n%d\n", Part1(file));
+        file.close();
+        file = new Scanner(Path.of("input.txt"));
+        System.out.printf("The password is:\n%d\n", Part2(file));
+        file.close();
+    }
+
+    static int Part1(Scanner file) {
         int position = 50;
         int password = 0;
-        Scanner file = new Scanner(Path.of("input.txt"));
         while (file.hasNext()) {
             String line = file.nextLine();
             char direction = line.charAt(0);
@@ -21,7 +29,7 @@ public class code{
                 default:
                     System.out.println("Invalid initial character: " + direction);
                     break;
-                }
+            }
             while (position < 0 || position > 99) {
                 if (position < 0) {
                     position += 100;
@@ -32,9 +40,47 @@ public class code{
             if (position == 0) {
                 password++;
             }
-            
         }
-        System.out.printf("The password is:\n%d", password);
-        file.close();
+        return password;
+    }
+
+    static int Part2(Scanner file) {
+        int lastPosition = 50;
+        int position = 50;
+        int password = 0;
+        while (file.hasNext()) {
+            String line = file.nextLine();
+            char direction = line.charAt(0);
+            int distance = Integer.parseInt(line.substring(1));
+            switch (direction) {
+                case 'L':
+                    lastPosition = position;
+                    position -= distance;
+                    break;
+                    case 'R':
+                    lastPosition = position;
+                    position += distance;
+                    break;
+                default:
+                    System.out.println("Invalid initial character: " + direction);
+                    break;
+            }
+            if (position == 0) {
+                password++;
+            }
+            while (position < 0 || position > 99) {
+                if (lastPosition != 0) {
+                    password++;
+                }
+                if (position < 0) {
+                    lastPosition = position;
+                    position += 100;
+                } else {
+                    lastPosition = position;
+                    position -= 100;
+                }
+            }
+        }
+        return password;
     }
 }
--- Day1/code.py
+++ Day1/code.py
@@ -1,7 +1,12 @@
-password = 0
-position = 50
-print(position)
-with open("input.txt","r") as f:
+def main():
+    with open("input.txt","r") as f:
+        print(f"The password is : \n{Part1(f)}")
+    with open("test.txt","r") as f:
+        print(f"The password is : \n{Part2(f)}")
+
+def Part1(f: list[str]) -> int:
+    password = 0
+    position = 50
     for line in f:
         direction, distance = line[0], int(line[1:])
         match direction.upper():
@@ -16,4 +21,30 @@ with open("input.txt","r") as f:
                 position -= 100
         if position == 0:
             password += 1
-print(f"The password is : \n{password}")
+    return password
+
+def Part2(f: list[str]) -> int:
+    password = 0
+    position = 50
+    last_position = 50
+    for line in f: # type: ignore
+        direction, distance = line[0], int(line[1:])
+        match direction.upper():
+            case "L":
+                last_position = position
+                position -= distance
+            case "R":
+                last_position = position
+                position += distance
+        if position == 0:
+            password += 1
+        while position < 0 or position > 99:
+            if last_position != 0:
+                password += 1
+            if position < 0:
+                position += 100
+            else:
+                position -= 100
+    return password
+
+main()
