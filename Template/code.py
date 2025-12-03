f1a66952bb36a040426fa3886f4e812ca123f816
Flame1611 <113800369+Flame1611@users.noreply.github.com>
2025-12-03 01:54:06 +0000

    Created Template Folder

---
+++ Template/code.py
@@ -0,0 +1,13 @@
+def main():
+    with open("input.txt","r") as f:
+        print(f"Answer for Part 1:\n{part1(f)}") 
+    with open("input.txt","r") as f:
+        print(f"Answer for Part 1:\n{part2(f)}") 
+
+def part1(f):
+    pass
+
+def part2(f):
+    pass
+
+main()
